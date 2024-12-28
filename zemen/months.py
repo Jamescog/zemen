a7c7fdaea7f16f1b8c482b68bbdf9d3c5a378d39
Jamescog <jamescog72@gmail.com>
2024-12-28 03:23:31 +0300

    chore: add docstring

--- zemen/months.py
+++ zemen/months.py
@@ -1,4 +1,30 @@
-"""Contains Names of Months with different languages"""
+"""
+This module contains the names of months in different languages.
+
+Currently, it includes:
+- English (en)
+- Amharic (am)
+
+The `months` dictionary maps month numbers as strings to
+their corresponding month names in each language.
+
+Structure:
+months = {
+    'en': {
+        "1": "September", "2": "October", "3": "November",
+        "4": "December", "5": "January", "6": "February",
+        "7": "March", "8": "April", "9": "May", "10": "June",
+        "11": "July", "12": "August"
+    },
+    'am': {
+        "1": "መስከረም", "2": "ጥቅምት", "3": "ህዳር",
+        "4": "ታህሳስ", "5": "ጥር", "6": "የካቲት",
+        "7": "መጋቢት", "8": "ሚያዝያ", "9": "ግንቦት",
+        "10": "ሰኔ", "11": "ሐምሌ", "12": "ነሐሴ",
+        "13": "ጳጉሜ"
+    }
+}
+"""
 
 months = {
     'en': {
